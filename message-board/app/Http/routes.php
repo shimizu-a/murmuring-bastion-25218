249de0f7b84fdd9a5e0912e95aa9124a8d5f5195
shimizu <ako.shimizu@gmail.com>
2017-07-06 11:22:11 +0000

    July 6th work

--- message-board/app/Http/routes.php
+++ message-board/app/Http/routes.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+Route::resource('messages','MessagesController');
+
+/*以下不要。。
+//CRUD
+Route::get('messages/{id}', 'MessagesController@show');
+Route::post('messages', 'MessagesController@store');
+Route::put('messages/{id}', 'MessagesController@update');
+Route::delete('messages/{id}', 'MessagesController@destroy');
+//index: show の補助ページ
+Route::get('messages','MessagesController@index');
+//create:新規作成用のフォームページ
+Route::get('messages/create','MessagesController@create');
+//edit: 更新用のフォームページ
+Route::get('messages/{id}/edit','MessagesController@edit');
+*/
