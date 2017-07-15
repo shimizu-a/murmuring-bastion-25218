8cb1af6ecdad006fcc447af5b808278781261618
shimizu <ako.shimizu@gmail.com>
2017-07-15 14:16:02 +0000

    Procfile

---
+++ message-board/resources/views/messages/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+ <h1>メッセージ新規作成ページ</h1>
+
+ @include('commons.error_messages')
+
+
+
+    {!! Form::model($message, ['route' => 'messages.store']) !!}
+
+        {!! Form::label('title', 'タイトル:') !!}
+        {!! Form::text('title') !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+
+@endsection
